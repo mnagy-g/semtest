f363607f4cb03040696847a913d8295be0a1a58c
Martin Nagy <martin.nagy@gjensidige.no>
2023-02-07 11:10:30 +0100

    feat: test

---
+++ release.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+        }
+      }
+    ]
+  ]
+};
