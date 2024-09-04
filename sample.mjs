08fb20cce0e3a27372349ef13891d2d67363618c
Martin Nagy <martin.nagy@gjensidige.no>
2024-09-04 21:59:46 +0200

    fix: tag names

--- sample.mjs
+++ sample.mjs
@@ -3,7 +3,7 @@ import { parseCommitsStream } from 'conventional-commits-parser';
 import { writeChangelogStream } from 'conventional-changelog-writer';
 import createPreset from 'conventional-changelog-conventionalcommits';
 
-export default async function doStuff(github, context) {
+export default async ({ github, context, tagName }) => {
   const { repo, owner } = context.repo;
 
   const { data: release } = await github.rest.repos.getLatestRelease({
@@ -16,7 +16,7 @@ export default async function doStuff(github, context) {
   const commits = await new Promise((resolve, reject) => {
     const result = [];
 
-    getRawCommitsStream({ from: release ? release.tag_name : '', to: 'v1.19.0' })
+    getRawCommitsStream({ from: release ? release.tag_name : '', to: tagName })
       .pipe(parseCommitsStream(preset.parser))
       .pipe(writeChangelogStream(undefined, preset.writer))
       .on('data', (data) => result.push(data))
@@ -24,5 +24,11 @@ export default async function doStuff(github, context) {
       .on('error', reject)
   });
 
-  console.log(commits.join('\n'));
+  await github.rest.repos.createRelease({
+    owner,
+    repo,
+    name: tagName,
+    tag_name: tagName,
+    body: commits.join('\n')
+  });
 }
